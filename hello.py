2809d176c94cf182d269cb1a42f32072c809853f
Isa Leite <isabela.leeite@hotmail.com>
2024-07-09 16:44:00 -0300

    upgrade SP and FR

--- hello.py
+++ hello.py
@@ -32,5 +32,9 @@ if current_language == "pt_BR":
     msg = "Olá, Mundo!"
 elif current_language == "it_IT":
     msg = "Ciao, Mondo!"
+elif current_language == "es_SP":
+    msg = "Hola, Mundo!"
+elif current_language == "fr-FR":
+    msg = "Bonjour Monde"
 
 print(msg)
