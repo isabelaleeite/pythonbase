b85c7d6cb991d5dca86092e23cdcf3554d1f71c6
Isa Leite <isabela.leeite@hotmail.com>
2024-07-10 23:44:29 -0300

    exercicio tabuada

--- string.py
+++ string.py
@@ -10,7 +10,7 @@ template % (nome, saldo)
 type(print("OLá, %s" % 1324))
  
 
- # New style
+ # str.format {}
 
 
 msg = " Olá, {} você é o player n {} e voce tem {:.3f} pontos".format("Bruno", 2, 987.3)
@@ -30,4 +30,16 @@ print(nome)
 nome = "{:#^11}".format("Isa")
 print(nome)
 
+# f-strings
+
+nome_2 = "Isa"
+texto = f"Olá, {nome_2}, seja bem vinde"
+
+print(texto)
+
+#unicode emoji
+
+print("\U0001F43C")
+
+print("\N{panda face}")
 
