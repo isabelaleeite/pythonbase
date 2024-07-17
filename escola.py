223a2380de0dd7ea29e7d06f791756c48e23c3a1
Isa Leite <isabela.leeite@hotmail.com>
2024-07-16 22:12:57 -0300

    Criação do exercício de relatório de crianças por atividade

---
+++ escola.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+ 
+"""Exibe relatório de crianças por atividade.
+
+Imprimir a lista de crianças agrupadas por sala que frequentas cada uma das atividades.
+ """
+
+__version__ = "0.1.0"
+
+sala1 = ["Erik", "Maia", "Gustavo", "Manuel", "Sofia", "Joana"]
+
+sala2 = ["Joao", "Antonio", "Carlos", "Maria", "Isa"]
+
+aula_ingles = ["Erik", "Maia", "Joana", "Carlos", "Antonio"]
+aula_musica = ["Erik", "Carlos", "Maria"]
+aula_danca = ["GUstavo", "Sofia", "Joana", "Antonio"]
+
