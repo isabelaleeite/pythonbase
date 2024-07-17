c133d5a9758a260fd70ae8bb1dc10e9423d4f241
Isa Leite <isabela.leeite@hotmail.com>
2024-07-16 23:38:12 -0300

    Exercicio com Sets

--- escola_v2_com_sets.py
+++ escola_v2_com_sets.py
@@ -5,7 +5,7 @@
 Imprimir a lista de crianças agrupadas por sala que frequentas cada uma das atividades.
  """
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 sala1 = ["Erik", "Maia", "Gustavo", "Manuel", "Sofia", "Joana"]
 
@@ -27,17 +27,15 @@ for nome_atividade, atividade in atividades:
     print(f"ALunos da atividade {nome_atividade}\n")
     print("-" * 50)
 
-    atividade_sala1 = []
-    atividade_sala2 = []
 
-    for aluno in atividade:
-        if aluno in sala1:
-            atividade_sala1.append(aluno)
-        elif aluno in sala2:
-            atividade_sala2.append(aluno)
+    # retornar os alunos da sala1 que tem interseção com a atividade
 
-    print(f"A {nome_atividade} sala1", atividade_sala1)
-    print(f"A {nome_atividade} sala2 ", atividade_sala2)
+    atividade_sala1 = set(sala1) & set(atividade)
+    atividade_sala2 = set(sala2).intersection(atividade)
+
+
+    print("Sala1", atividade_sala1)
+    print("Sala2 ", atividade_sala2)
    
     print("-" * 50 )
 
